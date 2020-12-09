ed2e2c16e62e4265acbbc1a8020b92afe1a513b0
laik <laik.lj@me.com>
2020-12-09 16:28:37 +0800

    fix cd crd define spec

--- pkg/controller/cd.go
+++ pkg/controller/cd.go
@@ -185,6 +185,7 @@ func (s *CDController) Run(addr string, stop <-chan struct{}) error {
 		obj, _, err := s.Apply(common.YceCloudExtensions, k8s.CD, name, unstructured, true)
 		if err != nil {
 			internalApplyErr(g, err)
+			fmt.Printf("cd controller apply error (%s)\n", err)
 			return
 		}
 
